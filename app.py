0473189c5dbc774851d941227abd2d7213a23505
Muhammad Faizan Naeem <84436536+muhammadfaizannaheem@users.noreply.github.com>
2022-04-08 21:27:38 +0500

    New API

---
+++ app.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from flask import Flask, request,jsonify,render_template
+import joblib
+import numpy as np
+
+app=Flask(__name__)
+
+model=joblib.load('KNN Trained and Saved Model.sav')
+
+# @app.route('/predicts',methods = ['POST','GET'])
+# def predicts():
+#     json = request.json
+#     query_df=pd.DataFrame(json)
+#     prediction = model.predict(query_df)
+#     return jsonify({'Prediction ': list(prediction) })
+@app.route('/predict',methods = ['POST'])
+def ping():
+    testtext=request.json
+    query_df=pd.DataFrame(testtext)
+    prediction = model.predict(query_df)
+    return jsonify({'Prediction ': int(prediction) })
+
+if __name__ == '__main__':
+    app.run(debug=True)
